81f7f679fec2ce376e802135af282832f5404a73
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-07-14 00:21:27 -0700

    feat(src): land initial prune-data task implementation

--- src/tasks/prune-data.ts
+++ src/tasks/prune-data.ts
@@ -11,7 +11,7 @@ import { skipListrTask, waitForListr2OutputReady } from 'universe:util.ts';
 
 import type DriveDb from '@nhscc/backend-drive/db';
 import type QoverflowDb from '@nhscc/backend-qoverflow/db';
-import type { WithId } from 'mongodb';
+import type { Document, ObjectId, WithId } from 'mongodb';
 import type { Promisable } from 'type-fest';
 import type { GlobalExecutionContext } from 'universe:configure.ts';
 import type { ActualTargetProblem } from 'universe:constant.ts';
@@ -416,127 +416,95 @@ export default async function task(
     debug('active pruner configuration: %O', limitsConfig);
 
     await Promise.all(
-      Object.entries(limitsConfig).map(async ([dbName, dbLimitsObj]) => {
-        debug(`using db "${dbName}"`);
+      Object.entries(limitsConfig).map(async ([targetPath, colLimitsObj]) => {
+        const [dbName, collectionName] = targetPath.split('.');
+
+        assert(dbName && collectionName);
+        debug('using db %O', dbName);
+
         const db = await getDb({ name: dbName });
 
-        await Promise.all(
-          Object.entries(dbLimitsObj).map(async ([collectionName, colLimitsObj]) => {
-            const name = `${dbName}.${collectionName}`;
-            debug(`collection "${name}" is a target for pruning`);
+        const name = `${dbName}.${collectionName}`;
+        debug('collection %O is a target for pruning', name);
 
-            const subLog = taskLog.extend(name);
-            const collection = db.collection(collectionName);
-            const totalCount = await collection.countDocuments();
+        const subLog = taskLog.extend(target).extend(name);
+        const collection = db.collection(collectionName);
+        const totalCount = await collection.countDocuments();
 
-            const {
-              limit: limitSpec,
-              orderBy = '_id',
-              deleteFn = undefined
-            } = colLimitsObj;
+        const { limit: limitSpec, orderBy = '_id', deleteFn = undefined } = colLimitsObj;
 
-            const pruneCollectionAtThreshold = async (
-              thresholdEntry: WithId<Document> | null,
-              deleteFn: DataLimit['deleteFn'],
-              pruneMessage: string,
-              noPruneMessage: string
-            ) => {
-              if (thresholdEntry) {
-                debug(`determined threshold entry: ${thresholdEntry._id}`);
-                let deletedCount: number;
+        debug('limiting metric: document size');
 
-                if (deleteFn) {
-                  debug('using custom pruning strategy');
-                  deletedCount = await deleteFn(thresholdEntry);
-                } else {
-                  debug('using default pruning strategy');
-                  deletedCount = (
-                    await collection.deleteMany({
-                      [orderBy]: { $lte: thresholdEntry[orderBy] }
-                    })
-                  ).deletedCount;
-                }
+        const { maxBytes } = limitSpec;
 
-                subLog(`${deletedCount} pruned (${pruneMessage})`);
-              } else {
-                subLog(`0 pruned (${noPruneMessage})`);
-              }
-            };
-
-            if ('maxBytes' in limitSpec) {
-              debug('limiting metric: document size');
-
-              const { maxBytes } = limitSpec;
-
-              debug(`sorting ${name} by "${orderBy}"`);
-              debug(
-                `iteratively summing document size until limit is reached (${maxBytes} bytes)`
-              );
-
-              // ? Use $bsonSize operator to sort by most recent first, then sum
-              // ? them until either documents are exhausted or total size >
-              // ? limit, then delete the (old) documents that exist beyond the
-              // ? limit.
-              const cursor = collection.aggregate<WithId<{ size: number }>>([
-                { $sort: { [orderBy]: -1 } },
-                { $project: { _id: true, size: { $bsonSize: '$$ROOT' } } }
-              ]);
-
-              let totalSizeBytes = 0;
-              let thresholdId: ObjectId | null = null;
-              let foundThresholdId = false;
-
-              (await cursor.toArray()).forEach(({ _id, size }) => {
-                if (!thresholdId) {
-                  thresholdId = _id;
-                }
-
-                totalSizeBytes += size;
-
-                if (!foundThresholdId) {
-                  if (totalSizeBytes > maxBytes) {
-                    foundThresholdId = true;
-                  } else {
-                    thresholdId = _id;
-                  }
-                }
-              });
-
-              await pruneCollectionAtThreshold(
-                foundThresholdId && thresholdId ? { _id: thresholdId } : null,
-                deleteFn,
-                `${totalCount}, ${totalSizeBytes}b > ${maxBytes}b`,
-                `${totalCount}, ${totalSizeBytes}b <= ${maxBytes}b`
-              ).then(() => cursor.close());
-            } else {
-              debug('limiting metric: document count');
-
-              const { maxDocuments } = limitSpec;
-
-              if (!maxDocuments) {
-                throw new Error(ErrorMessage.GuruMeditation());
-              }
-
-              debug(`sorting ${name} by "${orderBy}"`);
-              debug(`skipping ${maxDocuments} entries"`);
-
-              const cursor = collection
-                .find()
-                .sort({ [orderBy]: -1 })
-                .skip(maxDocuments)
-                .limit(1);
-
-              const thresholdEntry = await cursor.next();
-
-              await pruneCollectionAtThreshold(
-                thresholdEntry,
-                deleteFn,
-                `${totalCount} > ${maxDocuments}`,
-                `${totalCount} <= ${maxDocuments}`
-              ).then(() => cursor.close());
-            }
-          })
+        debug('sorting %O by %O', name, orderBy);
+        debug(
+          `iteratively summing document size until limit is reached (%O bytes)`,
+          maxBytes
         );
+
+        // ? Use $bsonSize operator to sort by most recent first, then sum
+        // ? them until either documents are exhausted or total size >
+        // ? limit, then delete the (old) documents that exist beyond the
+        // ? limit.
+        const cursor = collection.aggregate<WithId<{ size: number }>>([
+          { $sort: { [orderBy]: -1 } },
+          { $project: { _id: true, size: { $bsonSize: '$$ROOT' } } }
+        ]);
+
+        let totalSizeBytes = 0;
+        let thresholdId = null as ObjectId | null;
+        let foundThresholdId = false as boolean;
+
+        for await (const { _id, size } of cursor) {
+          totalSizeBytes += size;
+
+          if (!foundThresholdId) {
+            thresholdId = _id;
+
+            if (totalSizeBytes > maxBytes) {
+              foundThresholdId = true;
+              break;
+            }
+          }
+        }
+
+        await pruneCollectionAtThreshold(
+          foundThresholdId && thresholdId ? { _id: thresholdId } : null,
+          deleteFn,
+          `${totalCount}, ${totalSizeBytes}b > ${maxBytes}b`,
+          `${totalCount}, ${totalSizeBytes}b <= ${maxBytes}b`
+        ).then(() => cursor.close());
+
+        async function pruneCollectionAtThreshold(
+          thresholdEntry: WithId<Document> | null,
+          deleteFn: CollectionDataLimit['deleteFn'],
+          pruneMessage: string,
+          noPruneMessage: string
+        ) {
+          if (thresholdEntry) {
+            debug('determined threshold entry: %O', thresholdEntry._id);
+            let deletedCount: number;
+
+            if (deleteFn) {
+              debug('using custom pruning strategy');
+              deletedCount = await deleteFn(thresholdEntry);
+            } else {
+              debug('using default pruning strategy');
+              assert(thresholdEntry[orderBy] !== undefined);
+
+              deletedCount = (
+                await collection.deleteMany({
+                  [orderBy]: { $lte: thresholdEntry[orderBy] }
+                })
+              ).deletedCount;
+            }
+
+            subLog(`${deletedCount} pruned (${pruneMessage})`);
+          } else {
+            subLog(`0 pruned (${noPruneMessage})`);
+          }
+        }
       })
     );
   }
