d132e773debcac23e6715e49b583c7c4913f4f50
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-07-13 23:46:56 -0700

    refactor: integrate latest @-xun/mongo-schema

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,9 @@
 import { runProgram } from '@-xun/cli';
-import { setupForcedMultitenancyOverride } from '@-xun/mongo-schema';
+import { setupForcedMultitenancyOverride } from '@-xun/mongo-schema/multitenant';
 
 import type { GlobalExecutionContext } from 'universe:configure.ts';
 
-setupForcedMultitenancyOverride();
+setupForcedMultitenancyOverride('multitenant');
 
 /**
  * This is the simple CLI entry point executed directly by node.
--- test/setup.ts
+++ test/setup.ts
@@ -3,9 +3,13 @@
  **  bootstrapping the runtime for every test file.
  */
 
+import { setupForcedMultitenancyOverride } from '@-xun/mongo-schema/multitenant';
+
 // ? jest-extended will always come from @-xun/symbiote (i.e. transitively)
 // {@symbiote/notInvalid jest-extended}
 
 // ? https://github.com/jest-community/jest-extended#typescript
 import 'jest-extended';
 import 'jest-extended/all';
+
+setupForcedMultitenancyOverride('multitenant');
